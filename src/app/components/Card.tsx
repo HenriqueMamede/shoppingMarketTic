22612a4cac2504211f01ebfe77fa94f13c453725
Henrique Mamede <henrique.silva@venturus.org.br>
2024-02-07 15:19:59 -0300

    feat: Using product mockup to automatically render products

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,23 +1,27 @@
+import { Product } from "../interfaces/Produtc";
 import Button from "./Button";
 
-const Card = () => {
+const Card = ({ item }: Product) => {
   return (
-    <div className="w-64 rounded-2xl bg-blue-200 p-2">
-      <div>
+    <div
+      key={item.id}
+      className="flex h-96 w-64 flex-col justify-center rounded-2xl bg-white p-2"
+    >
+      <div className="flex justify-center">
         <img
-          src="http://localhost:5173/assets/products/tenis.jpg"
-          alt="Product image"
-          className="w-full rounded-t-lg object-cover"
+          src={`http://localhost:5173/assets/products/${item.image}.jpg`}
+          alt={item.name}
+          className="h-40 rounded-t-lg object-cover"
         />
       </div>
 
-      <div className="p-4">
+      <div className="flex flex-col gap-2 p-4">
         <div className="mb-2 flex items-center justify-center">
-          <h3 className="text-lg font-bold">Nome do Produto</h3>
+          <h3 className="text-center text-lg font-bold">{item.name}</h3>
         </div>
 
         <div className="flex items-center justify-center">
-          <span className="text-gray-700">R$ 149,99</span>
+          <span className="text-gray-700">R$ {item.price}</span>
         </div>
         <Button>Adicionar ao Carrinho</Button>
       </div>
--- src/app/views/Home/index.tsx
+++ src/app/views/Home/index.tsx
@@ -1,18 +1,16 @@
 import Card from "../../components/Card";
+import { mockProdutos } from "../../../mock/listaProdutos";
 
 const Home = () => {
   return (
     <div className="mb-36 mt-60 grid h-5/6 w-11/12 grid-cols-4 gap-4 overflow-x-auto">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {mockProdutos?.map((product) => {
+        return (
+          <div key={product.id}>
+            <Card item={product} />
+          </div>
+        );
+      })}
     </div>
   );
 };
