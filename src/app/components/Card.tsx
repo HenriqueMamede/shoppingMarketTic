b07a06109db49416ace42520f4577ac9185743a5
Henrique Mamede <henrique.silva@venturus.org.br>
2024-02-07 14:03:31 -0300

    feat: Add new 'Card' component

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,25 @@
+const Card = () => {
+  return (
+    <div className="w-64 rounded-2xl bg-blue-200 p-2">
+      <div>
+        <img
+          src="http://localhost:5173/assets/products/tenis.jpg"
+          alt="Product image"
+          className="w-full rounded-t-lg object-cover"
+        />
+      </div>
+
+      <div className="p-4">
+        <div className="mb-2 flex items-center justify-center">
+          <h3 className="text-lg font-bold">Nome do Produto</h3>
+        </div>
+
+        <div className="flex items-center justify-center">
+          <span className="text-gray-700">R$ 149,99</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
