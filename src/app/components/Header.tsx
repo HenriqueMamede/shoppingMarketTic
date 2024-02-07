44f5b76f6f543c2e0c6de110d261606665f25cb3
Henrique Mamede <henrique.silva@venturus.org.br>
2024-02-07 14:11:45 -0300

    refactor: Adding the Input component to component Header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,21 +1,19 @@
-import logo from "./assets/logo.png";
+import Input from "../components/Input";
 
 const Header = () => {
   return (
-    <header className="fixed right-0 top-0 flex w-full bg-white py-6">
+    <header className="fixed right-0 top-0 flex w-full bg-white py-3">
       <div className="mx-auto flex w-11/12 items-center justify-between gap-52">
         <div className="flex">
           <a href="/">
-            <img className="max-w-36" src={logo} alt="Company Logo" />
+            <img
+              className="max-w-36"
+              src="http://localhost:5173/assets/logo.png"
+              alt="Company Logo"
+            />
           </a>
         </div>
-        <div className="flex w-3/5 items-center">
-          <input
-            type="text"
-            className="w-full rounded-full border border-gray-600 px-4 py-2 text-black focus:outline-none"
-            placeholder="Search..."
-          />
-        </div>
+        <Input />
         <div className="flex items-center justify-between gap-12">
           <a href="/login" className="text-blue-500 hover:underline">
             Login
