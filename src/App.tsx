289d338d428f3cf11cc8efd565bdf90e7f9e94bd
Henrique Mamede <henrique.silva@venturus.org.br>
2024-02-07 14:18:40 -0300

    feat: Creating a home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from "./app/components/Header";
+import Home from "./app/views/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Header />
+        <Home />
+      </>
+    ),
+  },
+]);
+
 const App = () => {
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-200 text-slate-950"></div>
+    <div className="flex h-screen items-center justify-center bg-gray-200 text-slate-950">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
   );
 };
 
---
+++ src/app/views/Home/index.tsx
@@ -0,0 +1,20 @@
+import Card from "../../components/Card";
+
+const Home = () => {
+  return (
+    <div className="mb-36 mt-60 grid h-5/6 w-11/12 grid-cols-4 gap-4 overflow-x-auto">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default Home;
